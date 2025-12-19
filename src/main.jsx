cd4279ee7ded5a76f2a567a1d986e5b2c74e8ae6
Arte <quickattach0@gmail.com>
2025-12-19 23:01:25 +0000

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router";
+import { BrowserRouter, Routes, Route, Link } from "react-router";
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
@@ -7,9 +7,11 @@ createRoot(document.getElementById('root')).render(
   
  <BrowserRouter>
     <Routes>
-      <Route path="arabic/" element={<App />} />
+      
+      <Route path="arabic/core" element={<App />} />
       <Route path="arabic/el" element={<Element />} />
       <Route path="*" element={<div>not found</div>}/>
+      <Route path="arabic/" element={<> <Link to="core">-core</Link> <div>--data </div> <div>---pool </div></>}/>
     </Routes>
   </BrowserRouter>
 );
