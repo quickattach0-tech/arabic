297a7a5818107f63d903810fe54c29041c0cdd33
Arte <quickattach0@gmail.com>
2025-12-20 09:52:27 +0000

    backup

--- src/Element.jsx
+++ src/Element.jsx
@@ -2,58 +2,12 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-function Element() {
-  const [count, setCount] = useState(0);
-  const circle = (rad)=>(
-    <svg width="200" height="200" xmlns="http://www.w3.org/2000/svg">
-      <circle
-        cx= "100"
-        cy= "100"
-        r={rad}
-        fill="blue"
-        stroke="black"
-        strokeWidth="3"
-      />
-    </svg>
-  );
-  const pos = (leftPos, topPos, rad) => (
-    <div
-      style={{
-        position: "absolute",
-        top: topPos,
-        left: leftPos,
-      }}
-    >
-      {circle(rad)}
-    </div>
-  );
-  var frame;
-  var Z=50;
-  let canvas;
-  for (let i = 0; i < 100*count; i += 100) {
-    for (let j = 0; j < 100*count; j += 100) {
-     if (i >= 600 || j>=600){
-          Z = Z/2;
-      canvas = <div> {pos((i-300)*0.25, (j-300)*0.25, 12.5)} </div>;
-      }
-      else if (i >= 300 || j>=300){
-          Z = Z/2;
-      canvas = <div> {pos((i-300)*0.5, (j-300)*0.5, 25)} </div>;
-      }
-       else{
-      canvas = <div> {pos(i, j, 50)} </div>;
 
-      }
-      frame = (
-        <div>
-          <div>{frame}</div> <div> {canvas}</div>
-        </div>
-      );
-    }
-  }
+function App() {
+  const [count, setCount] = useState(0);
+ 
   return (
     <>
-      <div>{frame}</div>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -75,7 +29,7 @@ function Element() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  );
+      );
 }
 
-export default Element;
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -151,8 +151,8 @@ createRoot(document.getElementById('root')).render(
   </div>
 </>
 } />
-      <Route path="arabic/state" element={<App />} />
-      <Route path="arabic/pool" element={<Element />} />
+      <Route path="arabic/state" element={<Element />} />
+      <Route path="arabic/pool" element={<App />} />
       <Route path="*" element={<div>not found</div>}/>
       <Route path="arabic/" element={<> <div>💱 Menu </div>  <div><Link to="state">🍭 -- State </Link> </div> <div><Link to="pool">🎲 --- Pool </Link> </div> <div><Link to="core">🌋 - Core </Link></div> </>}/>
     </Routes>
